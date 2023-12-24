098b734e86b57c094088d916860fc9494ccb1250
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-12-24 18:37:50 +0300

    msPromise, googlePromise

---
+++ doAfter.js
@@ -0,0 +1,13 @@
+const doAfter = (time) => {
+  //1 var
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randValue = Math.random();
+      if (randValue > 0.1) {
+        resolve(randValue);
+      } else {
+        reject(randValue);
+      }
+    }, time * 1000);
+  });
+};
